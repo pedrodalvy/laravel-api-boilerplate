65fc8a07ecdb51bd826f2849a24872a30a0407d6
Pedro Dalvy <pedrodalvy@outlook.com>
2022-02-25 19:24:15 -0400

    Install and config Laravel JWT Auth

---
+++ app/Traits/Users/GetJWT.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Traits\Users;
+
+trait GetJWT
+{
+    public function getJWTIdentifier(): mixed
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(): array
+    {
+        return [];
+    }
+}
