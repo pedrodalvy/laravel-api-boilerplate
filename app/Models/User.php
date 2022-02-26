5fa9afbfd27482363f62e97e831f311aa9bf0498
Pedro Dalvy <pedrodalvy@outlook.com>
2022-02-26 10:11:53 -0400

    Refact User model and migration to use softDeletes

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use App\Traits\Auth\GetJWT;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable, GetJWT;
+    use HasFactory, Notifiable, GetJWT, SoftDeletes;
 
     protected $fillable = [
         'name',
